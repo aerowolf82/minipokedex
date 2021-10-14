74e73a479c5182155f4c2c6dfa04c974c84f0e32
Andrew Clapp <aerowolf82@gmail.com>
2021-10-14 18:00:34 -0400

    Barebones Skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Home from './Components/Home'
+import Header from './Components/Header'
+import { useEffect, useState } from 'react'
+
+
+async function getProducts() {
+  let res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+  let data = await res.json();
+  return data;
+}
+
 
 function App() {
+
+  let [pokeData, setPokeData] = useState();
+  
+  console.log(pokeData);
+  useEffect(() => {
+    let mounted = true;
+    getProducts()
+      .then(items => {
+        if(mounted) {
+          setPokeData(items.results)
+        }
+      })
+    return () => mounted = false;
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <nav className="Nav">
+            <Route path='/:page' component={Header} />
+            <Route exact path='/' component={Header} />          
+          </nav>
+        </header>
+
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
